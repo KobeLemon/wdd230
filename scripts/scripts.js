10ab329af88e018fd631260e9b6677a725f4af09
KobeLemon <kobe42lemon@outlook.com>
2023-01-09 05:12:17 -0700

    Built my WDD 230 Home Portal

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+// this finds the element with the id of "date"
+const date = document.querySelector("#date");
+
+// this finds the element with the id of "currentyear"
+const currentyear = document.querySelector("#currentyear");
+
+// this updates the innerHTML of the #date element to show the document's last modified date/time
+date.innerHTML = `Last Updated: ${document.lastModified}`;
+
+// this updates the innerHTML of the #currentyear element to show the current year dynamically
+currentyear.innerHTML = `&copy; ${new Date().getFullYear()} | Kobe Lemon | Utah, USA`;
