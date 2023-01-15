bf37b97e7906f8c4a2f7dbd563dc7de6375d0887
KobeLemon <kobe42lemon@outlook.com>
2023-01-14 17:42:07 -0700

    Changed styling to make the page better formatted

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,9 @@
 
 // this finds the element with the id of "date"
-const date = document.querySelector("#date");
+const last_modified = document.querySelector("#last_modified");
 
 // this finds the element with the id of "currentyear"
 const currentyear = document.querySelector("#currentyear");
 
 // this updates the innerHTML of the #date element to show the document's last modified date/time
-date.innerHTML = `Last Updated: ${document.lastModified}`;
-
-// this updates the innerHTML of the #currentyear element to show the current year dynamically
-currentyear.innerHTML = `&copy; ${new Date().getFullYear()} | Kobe Lemon | Utah, USA`;
+last_modified.innerHTML = `Last Updated: ${document.lastModified}`;
