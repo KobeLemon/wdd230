f77b51a090b4cbcdd7377c491433fb385af6a2a6
KobeLemon <kobe42lemon@outlook.com>
2023-01-28 05:38:24 -0700

    Created Chamber Home Page with media stylings

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,52 @@
+// open & close the hamburger menu when in small view
+function toggleMenu() {
+    console.log("Hamburger Button clicked")
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+    document.getElementById("navbar").classList.toggle("open");
+    console.log("Hamburger Menu opened");
+}
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
+
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+document.getElementById("currentDate").innerHTML = fulldate;
+
+// find the element with the id of "copyrightYear"
+const copyrightYear = document.querySelector(".copyrightYear");
+
+// update the innerHTML of the ".copyrightYear" element to show the current year dynamically
+copyrightYear.innerHTML = new Date().getFullYear();
+
+// find the element with the id of "date"
+const lastModified = document.querySelector(".lastModified");
+
+// update the innerHTML of the #date element to show the document's last modified date/time
+lastModified.innerHTML = document.lastModified;
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,11 @@
-// this finds the element with the id of "date"
-const last_modified = document.querySelector("#last_modified");
+// this finds the element with the class of "copyrightYear"
+const copyrightYear = document.querySelector(".copyrightYear");
 
-// this finds the element with the id of "currentyear"
-const currentyear = document.querySelector("#currentyear");
+// this updates the innerHTML of the ".copyrightYear" element to show the current year dynamically
+copyrightYear.innerHTML = new Date().getFullYear();
 
-// this updates the innerHTML of the #date element to show the document's last modified date/time
-last_modified.innerHTML = document.lastModified;
+// this finds the element with the class of "lastModified"
+const lastModified = document.querySelector(".lastModified");
+
+// this updates the innerHTML of the ".lastModified" element to show the document's last modified date/time
+lastModified.innerHTML = document.lastModified;
